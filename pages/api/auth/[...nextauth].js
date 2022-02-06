14e3f99ca38cd571f1e4ac5ec2acea8dadddaeff
Karn Yongsiriwit <karn.y@rsu.ac.th>
2022-02-06 08:40:00 +0700

    next-auth

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,42 @@
+import NextAuth from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+
+export default NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        username: { label: "Username", type: "text" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials, req) {
+        const res = await fetch("https://www.mecallapi.com/api/login", {
+          method: 'POST',
+          body: JSON.stringify(credentials),
+          headers: { "Content-Type": "application/json" }
+        })
+        const data = await res.json()
+        if (data.status == 'ok') {
+          return data.user
+        }
+        return null
+      }
+    })
+  ],
+  secret: "LlKq6ZtYbr+hTC073mAmAh9/h2HwMfsFo4hrfCx5mLg=",
+  NEXTAUTH_URL=https://next-login-cyan.vercel.app/,
+  callbacks: {
+    async jwt({ token, user, account }) {
+      if (account) {
+        token.accessToken = account.access_token
+        token.user = user
+      }
+      return token
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken
+      session.user = token.user
+      return session
+    }
+  }
+})
